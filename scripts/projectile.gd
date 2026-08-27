7e3d079bd8755ec98b0bd50948257b81123497cc
zomagames <erikocia@proton.me>
2026-08-27 19:02:28 +0200

    Update projectile to use random effect/bullet sprites

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -1,5 +1,13 @@
 extends Area2D
 
+# Multiple projectile/bullet sprites for variety
+var projectile_sprites = [
+	"res://sprites/Fire Effect and Bullet 16x16.png",
+	"res://sprites/Water Effect and Bullet 16x16.png",
+	"res://sprites/Purple Effect and Bullet 16x16.png",
+	"res://sprites/Green Effect and Bullet 16x16.png",
+]
+
 @export var damage = 10
 @export var speed = 400.0
 var velocity = Vector2.ZERO
@@ -8,6 +16,16 @@ const MAX_LIFETIME = 10.0  # Remove projectile after 10 seconds
 
 func _ready():
 	area_entered.connect(_on_area_entered)
+	
+	# Load random projectile sprite
+	var sprite = $Sprite2D
+	var random_projectile = projectile_sprites[randi() % projectile_sprites.size()]
+	var projectile_texture = load(random_projectile)
+	
+	if projectile_texture:
+		sprite.texture = projectile_texture
+	
+	print("[Projectile] Fired with sprite: %s" % random_projectile)
 
 func _physics_process(delta):
 	global_position += velocity * delta
